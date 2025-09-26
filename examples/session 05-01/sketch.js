619c9d4e7a4f4f5237778064fc3e99c20044429c
ianpokorny <ianpokorny@gmail.com>
2025-09-26 12:14:28 -0400

    Update sketch.js

--- examples/session 05-01/sketch.js
+++ examples/session 05-01/sketch.js
@@ -3,9 +3,6 @@
 //		Demo: working with image assets.		//
 ////////////////////////////////////////////
  
- Go to "files" tab (on OpenProcessing menu to the right-- click upper right icon to reveal menu if hidden).
- Upload image assets as indicated. 
- 
  Note the use of preload() in this sketch for loading assets before setup() is called. 
  */
 
